9117b20f722f7b5047c811c870d9ab714a92569c
Martin Willingham <martin.willingham@gmail.com>
2026-06-23 18:00:29 -0400

    removed the api folder

--- src/app/api/stories/route.ts
+++
@@ -1,7 +0,0 @@
-export const dynamic = 'force-static';
-import { stories } from '@/data/stories'
-import { NextResponse } from 'next/server'
-
-export async function GET() {
-  return NextResponse.json(stories)
-}
