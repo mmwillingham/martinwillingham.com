e7d84f5d171d73112dc71b9c943d991173f696d3
Martin Willingham <martin.willingham@gmail.com>
2026-06-29 07:48:51 -0400

    MI exxcerpt

--- src/content/matecumbe-island.ts
+++ src/content/matecumbe-island.ts
@@ -1,7 +1,11 @@
 export const matecumbeIslandContent = `
 ### Chapter 1
 
-The sun beat down on the coastal waters, casting a brilliant glare over the keys. 
+Pit-stopping at the Florida welcome center, I refilled my coffee, popped in Jimmy Buffett, and pressed the gas. I could’ve spent the entire twelve hours remembering, but I took her advice, facing the future with excitement. With wind at my back, I set the compass due south and raised the spinnaker. Only eight more hours to go.
 
-[Paste your opening excerpt or chapter for Matecumbe Island here...]
+I stopped at a turnpike service plaza a few hours later. While waiting in line to prepay for fuel, a glass case of rotisserie hot dogs beckoned me. I grabbed a napkin and forked one into a bun. A boy, standing behind his mom and staring at me, tugged her shirt and asked if he could have a hot dog. She consented without lifting her nose from a People magazine. He speared one, stuck it in a bun, then set it directly on the counter… no napkin. “Here, Mommy.”
+
+A minute later, she closed the magazine and saw the hot dog sitting on the counter. “Matthew, that’s disgusting.” She pitched it in the trashcan and dragged him outside. Then, while filling my gas tank, I saw the same boy tossing crackers to birds and chasing them away. When he got within twenty feet of a lagoon next to the station, his mother asked him to stop. The boy ignored her, giggling at the birds. Then the father asked him, and the boy ignored him, too, scampering a bit closer to the water. “Matthew!” the man roared. “Get away from the water. NOW!” He grabbed the boy’s hand and pulled him to the car.
+
+I shook my head, pitying the boy for having such over-protective parents. I pulled out and drove away, never noticing the sign in front of the lagoon, “Danger. Beware of Alligators.”
 `
